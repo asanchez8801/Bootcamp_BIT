18cad8355f43991a691e1a2a650f228f28dda6ef
Jose Albeiro Sánchez Ibañez <jasanchez8801@gmail.com>
2024-03-08 21:54:00 -0500

    javascript

---
+++ Javascript/desafios.js
@@ -0,0 +1,33 @@
+"user strict"
+
+/*const nombreusuario = prompt("ingrese su nombre");
+
+switch (nombreusuario){
+  case"":
+    console.log("hola");
+  break;
+  default:
+    console.log("hola " + nombreusuario);
+    break; 
+}*/
+
+//hacer un programa que pida dos numero al usuario y los sume
+
+const numero1 = prompt("ingrese un numero");
+alert ("el primer numero es " + numero1);
+
+const numero2 = prompt("ingrese el siguiente numero");
+alert ("el segundo numero es " + numero2);
+
+Number(numero1);
+typeof(numero1);
+console.log(numero1);
+Number(numero2);
+
+const sumanumero =(numero1+numero2);
+
+alert ("la suma de los numeros es:" , sumanumero);
+
+
+
+//hacer un programa que pida aparte nombre y apellido y muentre el nombre completo  
