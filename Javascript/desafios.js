1968f8f78e79404760fb84a0beb53fa6dc79f62b
Jose Albeiro Sánchez Ibañez <jasanchez8801@gmail.com>
2024-03-11 22:05:15 -0500

    Javascript

--- Javascript/desafios.js
+++ Javascript/desafios.js
@@ -1,33 +1,59 @@
 "user strict"
 
 /*const nombreusuario = prompt("ingrese su nombre");
-
 switch (nombreusuario){
   case"":
     console.log("hola");
   break;
   default:
     console.log("hola " + nombreusuario);
-    break; 
+    break;
 }*/
 
+//hacer un programa que pida aparte nombre y apellido y muestre el nombre completo  
+/* const nombre = prompt("Por favor ingrese su nombre");
+const apellido = prompt("Ingrese su apellido");
+console.log(nombre+" "+apellido);
+document.write(nombre+" "+apellido);
+document.write(`Su nombre completo es ${nombre}  ${apellido}`); */
+
 //hacer un programa que pida dos numero al usuario y los sume
+/* const numero1 = Number(prompt("ingrese el primer numero"));
+const numero2 = parseInt(prompt("ingrese el segundo numero"));
+console.log(numero1 + numero2);
+document.write(`la suma de ${numero1} y ${numero2} es: ${numero1+numero2}`); */
 
-const numero1 = prompt("ingrese un numero");
-alert ("el primer numero es " + numero1);
+//hacer un programa que reciba del usuario un numero y muestre si es cero o si es positivo o si es negativo 
 
-const numero2 = prompt("ingrese el siguiente numero");
-alert ("el segundo numero es " + numero2);
-
-Number(numero1);
-typeof(numero1);
+/* const numero1 = prompt ("Ingrese un numero, este puede se negativo");
 console.log(numero1);
-Number(numero2);
 
-const sumanumero =(numero1+numero2);
+if (numero1 == 0){
+    document.write("su numero es cero");
+} else if (numero1 > 0) {
+    document.write("su numero es positivo");
+} else {
+    document.write("su numero es negativo");
+}  */ 
 
-alert ("la suma de los numeros es:" , sumanumero);
+//hacer un programa que reciba del usuario 2 numeros y muestre cual es el número mayor y cual es el numero menor.
+
+let numero1 = prompt ("ingrese su primer numero");
+let numero2 = prompt ("ingrese su segundo numero");
+
+numero1 =parseInt(numero1);
+numero2 =parseInt(numero2);
+
+if (numero1 > numero2) {
+  document.write("el numero mayor es ", numero1);
+  document.write("el numero menor es ", numero2);
+
+} else if (numero2 > numero1){
+  document.write("el numero mayor es ", numero2);
+  document.write("el numero menor es ", numero1);
+}
 
 
 
-//hacer un programa que pida aparte nombre y apellido y muentre el nombre completo  
+
+
